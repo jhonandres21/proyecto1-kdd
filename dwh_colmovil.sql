b5dd88ebeed72cd5f58ebff8b03d481e6486dda9
luisfemm <luisfemm@outlook.com>
2014-10-10 21:26:17 -0500

    DWH Final
    Script para generar la DWH con:
    Fecha, Tiempo, Localización y Demografía.

--- dwh_colmovil.sql
+++ dwh_colmovil.sql
@@ -36,11 +36,6 @@ DROP TABLE IF EXISTS colmovil_dwh.llamada CASCADE;
 
 DROP DOMAIN IF EXISTS colmovil_dwh.dominio_tipo_id;
 DROP DOMAIN IF EXISTS colmovil_dwh.dominio_flag;
-DROP DOMAIN IF EXISTS colmovil_dwh.dominio_genero;
-DROP DOMAIN IF EXISTS colmovil_dwh.dominio_dia_noche;
-DROP DOMAIN IF EXISTS colmovil_dwh.dominio_periodo_dia;
-DROP DOMAIN IF EXISTS colmovil_dwh.dominio_periodo_anio;
-DROP DOMAIN IF EXISTS colmovil_dwh.dominio_estado_civil;
 DROP DOMAIN IF EXISTS colmovil_dwh.dominio_recarga;
 
 /*==============================================================*/
@@ -64,11 +59,6 @@ DROP SEQUENCE IF EXISTS colmovil_dwh.seq_localizacion;
 
 CREATE DOMAIN dominio_tipo_id CHAR (5) CHECK ( VALUE IN ( 'C.C.', 'C.E.' ) );
 CREATE DOMAIN dominio_flag CHAR (5) CHECK ( VALUE IN ( 'True', 'False' ) );
-CREATE DOMAIN dominio_genero CHAR (9) CHECK ( VALUE IN ( 'Masculino', 'Femenino' ) );
-CREATE DOMAIN dominio_dia_noche CHAR (5) CHECK ( VALUE IN ( 'dia', 'noche' ) );
-CREATE DOMAIN dominio_periodo_dia CHAR (20) CHECK ( VALUE IN ( 'mañana', 'mediodia', 'tarde', 'noche' ));
-CREATE DOMAIN dominio_periodo_anio CHAR (30) CHECK ( VALUE IN ( 'Vacaciones_Verano', 'Vacaciones_Invierno', 'Semana_Santa', 'Navidad' ) );
-CREATE DOMAIN dominio_estado_civil CHAR (9) CHECK ( VALUE IN ( 'Soltero', 'Divorciado', 'Unión Libre', 'Casado', 'Viudo' ) );
 CREATE DOMAIN dominio_recarga CHAR (30) CHECK ( VALUE IN ( 'Adelanto de saldo', 'Recarga Electronica', 'Tarjeta', 'Online') );
 
 /*==============================================================*/
@@ -80,7 +70,7 @@ CREATE DOMAIN dominio_recarga CHAR (30) CHECK ( VALUE IN ( 'Adelanto de saldo', 
 /*==============================================================*/
 
 CREATE SEQUENCE seq_cliente INCREMENT BY 1 START WITH 1;
-create table cliente
+CREATE TABLE cliente
 (
 SK_cliente BIGINT NOT NULL DEFAULT nextval('seq_cliente'::regclass),
 numero_id INTEGER NOT NULL ,
@@ -93,24 +83,33 @@ PRIMARY KEY (SK_cliente)
 );
 
 /*==============================================================*/
-/* Dimensión: Demografía */
+/* Dimensión: Demografia */
 /*==============================================================*/
 
 CREATE SEQUENCE seq_demografia INCREMENT BY 1 START WITH 1;
-create table demografia
-(
-SK_demografia BIGINT NOT NULL DEFAULT nextval('seq_demografia'::regclass),
-estado_civil dominio_estado_civil NOT NULL,
-estrato INTEGER NOT NULL CHECK (estrato BETWEEN 1 AND 6),
-genero dominio_genero NOT NULL,
-PRIMARY KEY (SK_demografia)
-);
+
+CREATE TABLE DEMOGRAFIA AS 
+SELECT DISTINCT ON (
+cliente.estado_civil, 
+cliente.estrato, 
+cliente.genero) 
+cliente.estado_civil, 
+cliente.estrato, 
+cliente.genero 
+FROM colmovil.cliente 
+ORDER BY cliente.genero;
+
+ALTER TABLE DEMOGRAFIA ADD SK_demografia 
+BIGINT DEFAULT NEXTVAL('seq_demografia'::regclass) PRIMARY KEY;
+ALTER TABLE DEMOGRAFIA ALTER COLUMN estado_civil SET NOT NULL;
+ALTER TABLE DEMOGRAFIA ALTER COLUMN estrato SET NOT NULL;
+ALTER TABLE DEMOGRAFIA ALTER COLUMN genero SET NOT NULL;
 
 /*==============================================================*/
 /* Dimensión: Plan Voz */
 /*==============================================================*/
 CREATE SEQUENCE seq_plan_voz INCREMENT BY 1 START WITH 1;
-create table plan_voz
+CREATE TABLE plan_voz
 (
 SK_plan_voz BIGINT NOT NULL DEFAULT nextval('seq_plan_voz'::regclass),
 tipo_plan_voz VARCHAR(100) NOT NULL,
@@ -123,7 +122,7 @@ PRIMARY KEY (SK_plan_voz)
 /*==============================================================*/
 
 CREATE SEQUENCE seq_plan_datos INCREMENT BY 1 START WITH 1;
-create table plan_datos
+CREATE TABLE plan_datos
 (
 SK_plan_datos BIGINT NOT NULL DEFAULT nextval('seq_plan_datos'::regclass),
 nombre_plan_datos VARCHAR(100) NOT NULL,
@@ -136,13 +135,13 @@ PRIMARY KEY (SK_plan_datos)
 /*==============================================================*/
 
 CREATE SEQUENCE seq_equipo INCREMENT BY 1 START WITH 1;
-create table equipo
+CREATE TABLE equipo
 (
 SK_equipo BIGINT NOT NULL DEFAULT nextval('seq_equipo'::regclass),
 id_equipo BIGINT NOT NULL,
 marca VARCHAR(50) NOT NULL,
 modelo VARCHAR(50) NOT NULL,
-precio NUMERIC NOT NULL,
+precio INTEGER NOT NULL,
 PRIMARY KEY (SK_equipo)
 );
 
@@ -151,7 +150,7 @@ PRIMARY KEY (SK_equipo)
 /*==============================================================*/
 
 CREATE SEQUENCE seq_sim_card INCREMENT BY 1 START WITH 1;
-create table sim_card
+CREATE TABLE sim_card
 (
 SK_sim_card BIGINT NOT NULL DEFAULT nextval('seq_sim_card'::regclass),
 numero_serie BIGINT NOT NULL,
@@ -163,7 +162,7 @@ PRIMARY KEY (SK_sim_card)
 /* Dimensión: Oficina */
 /*==============================================================*/
 CREATE SEQUENCE seq_oficina INCREMENT BY 1 START WITH 1;
-create table oficina
+CREATE TABLE oficina
 (
 SK_oficina BIGINT NOT NULL DEFAULT nextval('seq_oficina'::regclass),
 direccion VARCHAR(200) NOT NULL,
@@ -175,59 +174,118 @@ PRIMARY KEY (SK_oficina)
 /*==============================================================*/
 
 CREATE SEQUENCE seq_tiempo INCREMENT BY 1 START WITH 1;
-create table tiempo
-(
-SK_tiempo BIGINT NOT NULL DEFAULT nextval('seq_tiempo'::regclass),
-tiempo TIME WITHOUT TIME ZONE NOT NULL,
-hora TIME WITHOUT TIME ZONE NOT NULL,
-cuarto_de_hora INTEGER NOT NULL,
-minuto INTEGER NOT NULL,
-periodo_del_dia dominio_periodo_dia NOT NULL,
-dia_noche_flag dominio_dia_noche NOT NULL,
-PRIMARY KEY (SK_tiempo)
+
+CREATE TABLE TIEMPO 
+AS (
+SELECT TO_CHAR(minute, 'hh24:mi') AS Tiempo,
+EXTRACT(hour FROM minute) AS hora,
+TO_CHAR(minute - (EXTRACT(minute FROM minute)::INTEGER % 15 || 'minutes')::INTERVAL, 'hh24:mi') || ' – ' || TO_CHAR(minute - (EXTRACT(minute FROM minute)::INTEGER % 15 || 'minutes')::INTERVAL + '14 minutes'::INTERVAL, 'hh24:mi') 
+AS cuarto_de_hora,
+EXTRACT(hour FROM minute)*60 + EXTRACT(minute FROM minute) AS minuto,
+CASE WHEN TO_CHAR(minute, 'hh24:mi') BETWEEN '06:00' AND '08:29'
+THEN 'Morning'
+WHEN TO_CHAR(minute, 'hh24:mi') BETWEEN '08:30' AND '11:59'
+THEN 'AM'
+WHEN TO_CHAR(minute, 'hh24:mi') BETWEEN '12:00' AND '17:59'
+THEN 'PM'
+WHEN TO_CHAR(minute, 'hh24:mi') BETWEEN '18:00' AND '22:29'
+THEN 'Evening'
+ELSE 'Night'
+END AS periodo_del_dia,
+CASE WHEN TO_CHAR(minute, 'hh24:mi') BETWEEN '07:00' AND '19:59' THEN 'Day'
+ELSE 'Night'
+END AS dia_noche
+FROM (SELECT '0:00'::time + (sequence.minute || ' minutes')::INTERVAL AS minute
+FROM generate_series(0,1439) AS sequence(minute)
+GROUP BY sequence.minute
+) DQ
+ORDER BY 1
 );
+
+ALTER TABLE TIEMPO ADD SK_tiempo BIGINT DEFAULT NEXTVAL('seq_tiempo'::regclass) PRIMARY KEY;
+ALTER TABLE TIEMPO ALTER COLUMN tiempo SET NOT NULL;
+ALTER TABLE TIEMPO ALTER COLUMN hora SET NOT NULL;
+ALTER TABLE TIEMPO ALTER COLUMN cuarto_de_hora SET NOT NULL;
+ALTER TABLE TIEMPO ALTER COLUMN minuto SET NOT NULL;
+ALTER TABLE TIEMPO ALTER COLUMN periodo_del_dia SET NOT NULL;
+ALTER TABLE TIEMPO ALTER COLUMN dia_noche SET NOT NULL;
 
 /*==============================================================*/
 /* Dimensión: Fecha */
 /*==============================================================*/
 
 CREATE SEQUENCE seq_fecha INCREMENT BY 1 START WITH 1;
-create table fecha
-(
-SK_fecha BIGINT NOT NULL DEFAULT nextval('seq_fecha'::regclass),
-fecha DATE NOT NULL,
-anio INTEGER NOT NULL,
-mes INTEGER NOT NULL,
-nombre_mes VARCHAR (20) NOT NULL,
-dia INTEGER NOT NULL,
-dia_del_anio INTEGER NOT NULL,
-nombre_dia VARCHAR (10),
-numero_semana INTEGER NOT NULL,
-fecha_formateada DATE NOT NULL,
-trimestre INTEGER NOT NULL,
-mes_del_anio INTEGER NOT NULL,
-fin_de_semestre dominio_flag,
-dia_festivo dominio_flag NOT NULL,
-periodo dominio_periodo_anio NOT NULL,
-fecha_comienzo_semana dominio_flag NOT NULL,
-fecha_fin_semana dominio_flag NOT NULL,
-fecha_comienzo_mes dominio_flag NOT NULL,
-PRIMARY KEY (SK_fecha)
+
+CREATE TABLE FECHA
+AS (
+SELECT
+datum AS Fecha,
+EXTRACT(year FROM datum) AS Anio,
+EXTRACT(month FROM datum) AS Mes,
+TO_CHAR(datum, 'TMMonth') AS Nombre_Mes,
+EXTRACT(day FROM datum) AS Dia,
+EXTRACT(doy FROM datum) AS Dia_De_Anio,
+TO_CHAR(datum, 'TMDay') AS Nombre_Dia,
+TO_CHAR(datum, 'yyyy/mm') AS Mes_Del_Anio,
+TO_CHAR(datum, 'iyyy/IW') AS Semana_Del_Anio,
+-- Fin de Semana
+CASE WHEN EXTRACT(isodow FROM datum) IN (6, 7) THEN 'Fin de Semana' ELSE 'Día de la
+Semana' END AS Fin_De_Semana,
+CASE WHEN TO_CHAR(datum, 'MMDD') IN ('0101', '0106', '0324', '0417', '0418', '0501', '0602',
+'0623', '0630', '0720', '0807', '0818', '1013', '1103', '1117', '1208', '1225')
+THEN 'Festivo' ELSE 'No Festivo' END
+AS Festivos_Colombia,
+CASE WHEN TO_CHAR(datum, 'MMDD') BETWEEN '0413' AND '0419' THEN 'Semana Santa'
+WHEN TO_CHAR(datum, 'MMDD') BETWEEN '1216' AND '1225' THEN 'Navidad'
+ELSE 'Normal' END
+AS Periodo,
+datum + (1 - EXTRACT(day FROM datum))::INTEGER AS Inicio_De_Mes,
+(datum + (1 - EXTRACT(day FROM datum))::INTEGER + '1 month'::INTERVAL)::date - '1 day'::INTERVAL
+AS Fin_De_Mes
+FROM (
+SELECT '2000-01-01'::DATE + sequence.day AS datum
+FROM generate_series(0,5475) AS sequence(day)
+GROUP BY sequence.day
+) DQ
+ORDER BY 1
 );
+
+ALTER TABLE FECHA ADD SK_fecha BIGINT DEFAULT NEXTVAL('seq_fecha'::regclass) PRIMARY KEY;
+ALTER TABLE FECHA ALTER COLUMN fecha SET NOT NULL;
+ALTER TABLE FECHA ALTER COLUMN anio SET NOT NULL;
+ALTER TABLE FECHA ALTER COLUMN mes SET NOT NULL;
+ALTER TABLE FECHA ALTER COLUMN nombre_mes SET NOT NULL;
+ALTER TABLE FECHA ALTER COLUMN dia SET NOT NULL;
+ALTER TABLE FECHA ALTER COLUMN dia_de_anio SET NOT NULL;
+ALTER TABLE FECHA ALTER COLUMN nombre_dia SET NOT NULL;
+ALTER TABLE FECHA ALTER COLUMN mes_del_anio SET NOT NULL;
+ALTER TABLE FECHA ALTER COLUMN semana_del_anio SET NOT NULL;
+ALTER TABLE FECHA ALTER COLUMN fin_de_semana SET NOT NULL;
+ALTER TABLE FECHA ALTER COLUMN festivos_colombia SET NOT NULL;
+ALTER TABLE FECHA ALTER COLUMN periodo SET NOT NULL;
+ALTER TABLE FECHA ALTER COLUMN inicio_de_mes SET NOT NULL;
+ALTER TABLE FECHA ALTER COLUMN fin_de_mes SET NOT NULL;
+
 
 /*==============================================================*/
 /* Dimensión: Localización					*/
 /*==============================================================*/
 
 CREATE SEQUENCE seq_localizacion INCREMENT BY 1 START WITH 1;
-create table localizacion
-(
-SK_localizacion BIGINT NOT NULL DEFAULT nextval('seq_localizacion'::regclass),
-departamento VARCHAR (30) NOT NULL,
-ciudad VARCHAR (30) NOT NULL,
-PRIMARY KEY (SK_localizacion)
-);
 
+CREATE TABLE LOCALIZACION AS 
+SELECT DISTINCT ON (
+localizacion.departamento, 
+localizacion.ciudad) 
+localizacion.departamento, 
+localizacion.ciudad 
+FROM colmovil.localizacion 
+ORDER BY localizacion.departamento;
+
+ALTER TABLE LOCALIZACION ADD SK_localizacion 
+BIGINT DEFAULT NEXTVAL('seq_localizacion'::regclass) PRIMARY KEY;
+ALTER TABLE LOCALIZACION ALTER COLUMN departamento SET NOT NULL;
+ALTER TABLE LOCALIZACION ALTER COLUMN ciudad SET NOT NULL;
 /*==============================================================*/
 /* Creación: Hechos						*/
 /*==============================================================*/
@@ -236,7 +294,7 @@ PRIMARY KEY (SK_localizacion)
 /* Hecho: Retiros						*/
 /*==============================================================*/
 
-create table retiro
+CREATE TABLE retiro
 (
 fecha BIGINT references fecha (SK_fecha),
 cliente BIGINT references cliente (SK_cliente),
@@ -249,7 +307,7 @@ plan_datos BIGINT references plan_datos (SK_plan_datos)
 /* Hecho: Recargas */
 /*==============================================================*/
 
-create table recarga
+CREATE TABLE recarga
 (	
 fecha BIGINT references fecha (SK_fecha),
 id_sim_card BIGINT references sim_card (SK_sim_card),
@@ -261,7 +319,7 @@ tipo_recarga BIGINT NOT NULL
 /* Hecho: Ventas */
 /*==============================================================*/
 
-create table venta
+CREATE TABLE venta
 (
 fecha BIGINT references fecha (SK_fecha),
 localizacion BIGINT REFERENCES localizacion (SK_localizacion),
@@ -281,7 +339,7 @@ total NUMERIC NOT NULL
 /* Hecho: Llamadas */
 /*==============================================================*/
 
-create table llamada
+CREATE TABLE llamada
 (
 fecha BIGINT references fecha (SK_fecha),
 localizacion BIGINT references localizacion (SK_localizacion),
