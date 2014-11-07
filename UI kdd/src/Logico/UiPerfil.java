d3222d08c5b23b8c457f312dcfeacd6068846aa9
jhonandres21 <jhonandresmedina@gmail.com>
2014-11-06 23:02:09 -0500

    Interfaz gráfica - Inicial

---
+++ UI kdd/src/Logico/UiPerfil.java
@@ -0,0 +1,40 @@
+package Logico;
+
+import javax.swing.JCheckBox;
+
+/**
+ *
+ * @author john
+ */
+/*
+ * En esta clase se van a poner todos los componentes de la interfaz gráfica
+ * que estén relacionados con las preguntas sobre "Perfiles"
+ */
+public class UiPerfil {
+
+    JCheckBox checkBoxSexoFemenino, checkBoxSexoMasculino;
+
+    public UiPerfil() {
+
+        checkBoxSexoFemenino = new JCheckBox("Femenino");
+        checkBoxSexoMasculino = new JCheckBox("Masculino");
+        inicializarCheckBox(checkBoxSexoFemenino);
+        inicializarCheckBox(checkBoxSexoMasculino);
+    }
+
+    private void inicializarCheckBox(JCheckBox checkBox) {
+
+        checkBox.setVisible(true);
+        checkBox.setSize(100, 45);
+        //margin bottom
+        checkBox.setMargin(new java.awt.Insets(0, 0, 10, 0));
+    }
+
+    public JCheckBox getCheckBoxSexoFemenino() {
+        return checkBoxSexoFemenino;
+    }
+
+    public JCheckBox getCheckBoxSexoMasculino() {
+        return checkBoxSexoMasculino;
+    }
+}
